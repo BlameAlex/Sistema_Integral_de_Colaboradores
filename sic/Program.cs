ed6ed8892f73fec2bdf354d1df0c5a201c7771f5
adrianjcl <j.adriancrdcld@gmail.com>
2025-07-14 09:42:26 -0500

    init

---
+++ sic/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using sic.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<SicDbContext>(options =>
+{
+    var cs = builder.Configuration.GetConnectionString("SicConnection");
+    options.UseMySQL(cs);
+});
+
+
+//Creacion de la app
+var app = builder.Build();
+
+//Creacion de la base de datos
+await using (var scope = app.Services.CreateAsyncScope())
+await using (var dbContext = scope.ServiceProvider.GetRequiredService<SicDbContext>())
+{
+    
+    await dbContext.Database.EnsureCreatedAsync();
+}
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
